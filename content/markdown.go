09d8143538d93dda6e70e75c35f4f345328a40f3
Shinichi Kogai <kogai@users.noreply.github.com>
2020-12-06 16:42:28 +0900

    Fix incosistant diff (#19)
    * add test which to convert nested list
    
    * HTML minifying might be make converter deterministic
    
    * Those diffs are may reasonable
    
    * use table driven test
    
    * extract converting method
    
    * attempt to avoid using -

--- content/markdown.go
+++ content/markdown.go
@@ -1,7 +1,12 @@
 package content
 
 import (
+	"bytes"
+
 	md "github.com/JohannesKaufmann/html-to-markdown"
+	"github.com/gomarkdown/markdown"
+	"github.com/tdewolff/minify/v2"
+	h "github.com/tdewolff/minify/v2/html"
 )
 
 func htmlToMarkdown(html string) (string, error) {
@@ -9,10 +14,22 @@ func htmlToMarkdown(html string) (string, error) {
 		HorizontalRule: "---",
 	}
 
+	m := minify.New()
+	m.AddFunc("text/html", h.Minify)
+	buf := bytes.NewBufferString("")
+	if err := m.Minify("text/html", buf, bytes.NewReader([]byte(html))); err != nil {
+		return "", err
+	}
+
 	converter := md.NewConverter("", true, opt)
-	markdownStr, err := converter.ConvertString(html)
+	markdownStr, err := converter.ConvertString(buf.String())
 	if err != nil {
 		return "", err
 	}
 	return markdownStr + "\n", nil
+}
+
+func markdownToHTML(rawmd string) (string, error) {
+	html := string(markdown.ToHTML([]byte(rawmd), nil, nil))
+	return html, nil
 }
